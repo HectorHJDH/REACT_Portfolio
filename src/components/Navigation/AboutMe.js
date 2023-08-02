1254357269596bdbe4aca9fcedf556bfe1216512
HectorJosueDH <hectorjosue78@gmail.com>
2023-08-02 01:25:15 -0600

    Moved img to assets folder

--- src/components/Navigation/AboutMe.js
+++ src/components/Navigation/AboutMe.js
@@ -1,23 +1,24 @@
 import React from "react";
 import * as styles from "../../styles";
-
 export default function AboutMe() {
   return (
     <div style={styles.pageStyle}>
       <h1 style={styles.titleStyle}>About Me</h1>
-      <img src={process.env.PUBLIC_URL + '/Hector_cut.jpg'} alt="Hector" style={styles.pictureStyle} />
-      <p style={styles.contentStyle}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+      <img
+        src={require('../../Assets/Hector_cut.jpg')}
+        alt="Hector"
+        style={styles.pictureStyle}
+      />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit,
+      lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi ac dui
+      fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras non risus
+      mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet ac. Sed nec
+      nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
+      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec
+      nec ornare elit. Donec blandit est sed risus feugiat porttitor. Vestibulum
+      molestie hendrerit massa non consequat. Vestibulum vitae lorem tortor. In
+      elementum ultricies tempus. Interdum et malesuada fames ac ante ipsum
+      primis in faucibus.
     </div>
   );
 }
